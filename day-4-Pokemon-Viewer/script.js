a829b8149b8dc83e9bcd8820dde258f0d172ffec
shyuuun <m4rshm4ll0w14@gmail.com>
2024-01-04 05:36:57 +0800

    displays info about the pokemon

--- day-4-Pokemon-Viewer/script.js
+++ day-4-Pokemon-Viewer/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const fetchData = async() => {
     const pkmnGetName = document.getElementById('enterPokemon').value.toLowerCase();
     const pkmnDescription = document.getElementById('pkmn-description');
@@ -10,20 +5,59 @@ const fetchData = async() => {
     const pkmnNumber = document.getElementById('pkmn-dex-no');
     const pkmnNormal = document.getElementById('pkmn-sprite-normal');
     const pkmnShiny = document.getElementById('pkmn-sprite-shiny');
+    const pkmnWeight = document.getElementById('pkmn-height');
+    const pkmnHeight = document.getElementById('pkmn-weight');
+    const pkmnAbility = document.getElementById('pkmn-ability');
+    const pkmHideAbility = document.getElementById('pkmn-hide-ability');
     
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnGetName}`);
-
+    const pkmnResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnGetName}`);
+    const pkmnSpeciesRes = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pkmnGetName}`)
     console.log(pkmnGetName);
 
     try {
-        if(!response.ok) {
+        if(!pkmnResponse.ok || !pkmnSpeciesRes.ok) {
             pkmnDescription.innerHTML = "No Pokemon Found";
             throw new Error("Couldn't not fetch resource");
         } 
-        const data = await response.json();
-        console.log(data);
+        const pkmnData = await pkmnResponse.json();
+        const pkmnSpeciesData = await pkmnSpeciesRes.json();
+
+        const getName = pkmnData.name;
+        const getNumber = pkmnData.id;
+        const getNormal = pkmnData.sprites.front_default;
+        const getShiny = pkmnData.sprites.front_shiny
+        const getWeight = pkmnData.weight;
+        const getHeight = pkmnData.height;
+        const getFlavorTxt = pkmnSpeciesData.flavor_text_entries[0].flavor_text;
+        const getAbility = pkmnData.abilities[0].ability.name;
+        const getHiddenAbility = pkmnData.abilities[1].ability.name;
+
+        console.log(getName);
+        console.log(getNumber);
+        console.log(getNormal);
+        console.log(getShiny);
+        console.log(getWeight);
+        console.log(getHeight);
+        console.log(getFlavorTxt);
+
+        pkmnName.innerHTML = getName;
+        pkmnNumber.innerHTML = getNumber;
+        pkmnNormal.src = getNormal;
+        pkmnShiny.src = getShiny;
+        pkmnWeight.innerHTML = getWeight;
+        pkmnHeight.innerHTML = getHeight;
+        pkmnAbility.innerHTML = getAbility;
+        pkmHideAbility.innerHTML = getHiddenAbility;
+        pkmnDescription.innerHTML = getFlavorTxt;
 
 
+        const viewCard = document.querySelectorAll('.sectionContain');
+
+        viewCard.forEach(i => i.style.display = "flex");
+
+
+
+        
     } catch(error) {
         console.error(error);
     }
