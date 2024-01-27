6a050de2a7e41b8d5df3d4d9beeb4cae8c7302c1
shyuuun <m4rshm4ll0w14@gmail.com>
2024-01-28 01:19:42 +0800

    add function of sorting cards

--- 7 - Shop/script.js
+++ 7 - Shop/script.js
@@ -1,4 +1,5 @@
 
+// to our responsive navbar
 const responsive = () => {
     
     const navBar = document.querySelector('nav');
@@ -9,7 +10,42 @@ const responsive = () => {
     } else {
         navBar.classList.remove('responsive');
     }
-
-
 }
+
+// for our sorting items 
+const cards = document.querySelectorAll('.card');
+const sortItem = (value) => {
+    switch(value) {
+        case 'featured':
+            console.log('Featured selected');
+            cards.forEach(card => {
+                if(!card.querySelector('.tag.featured')){
+                    card.style.display = 'none';
+                } else { 
+                    card.style.display = 'flex'
+                }
+            });
+            break;
+        case 'sale':
+            console.log('Sale selected');
+            cards.forEach(card => {
+                if(!card.querySelector('.tag.on-sale')){
+                    card.style.display = 'none';
+                } else { 
+                    card.style.display = 'flex'
+                }
+            });
+            break;
+        default: {
+            cards.forEach(card => {
+                card.style.display = 'flex';
+            });
+        }
+    }
+}
+
+
+
+
+
 
