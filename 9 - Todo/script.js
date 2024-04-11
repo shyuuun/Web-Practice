ffb9b62d717420c7a2eec123a3694d734687869a
shyuuun <m4rshm4ll0w14@gmail.com>
2024-04-12 01:33:08 +0800

    todo improvement on events

--- 9 - Todo/script.js
+++ 9 - Todo/script.js
@@ -1,12 +1,12 @@
 const exampleData = [
 	{
 		id: 0,
-		task: 'A sample task 1',
+		task: "A sample task 1",
 		isCompleted: true,
 	},
 	{
 		id: 1,
-		task: 'A sample task 2',
+		task: "A sample task 2",
 		isCompleted: false,
 	},
 ];
@@ -24,45 +24,28 @@ function displayData() {
 			cardTasks.innerHTML += `
             <div class="card__tasks">
                 <div class="card__desc">
-                    <input type="checkbox" name="" id="checkBox" data-id="${data.id}" ${data.isCompleted === true ? 'checked' : ""}>
+                    <input type="checkbox" name="" id="checkBox" data-id="${
+						data.id
+					}" ${data.isCompleted === true ? "checked" : ""}>
                     <input class="card__input-box hidden" type="text" name=" ">
-                    <p class="card__tasks_desc ${data.isCompleted === true ? 'completed' : ''}" >
+                    <p class="card__tasks_desc ${
+						data.isCompleted === true ? "completed" : ""
+					}" >
                         ${data.task}
                     </p>
                 </div>
                 <div class="card__btn__group">
                     <!-- edit button -->
-                    <button class="card__btn small" id="editBtn" data-id="${data.id}"><i class='bx bxs-edit-alt'></i></button>
+                    <button class="card__btn small" id="editBtn" data-id="${
+						data.id
+					}"><i class='bx bxs-edit-alt'></i></button>
                     <!-- delete button -->
-                    <button class="card__btn small" id="deleteBtn" data-id="${data.id}"><i class='bx bxs-trash'></i></button>
+                    <button class="card__btn small" id="deleteBtn" data-id="${
+						data.id
+					}"><i class='bx bxs-trash'></i></button>
                 </div>
             </div>        
             `;
-
-			const deleteBtns = document.querySelectorAll("#deleteBtn");
-
-			deleteBtns.forEach((deleteBtn) => {
-				deleteBtn.addEventListener("click", () => {
-					const id = parseInt(deleteBtn.dataset.id);
-					deleteItem(id);
-				});
-			});
-
-			const checkBoxes = document.querySelectorAll('#checkBox');
-			checkBoxes.forEach((checkbox => {
-				checkbox.addEventListener('change', e => {
-					const id = parseInt(checkbox.dataset.id);
-					if(e.target.checked === true) { 
-						updateCheckItem(id, true);
-					} else { 
-						updateCheckItem(id, false);
-					}
-
-					
-				})
-			}))
-			
-
 
 		});
 	} else {
@@ -73,6 +56,28 @@ function displayData() {
         `;
 	}
 }
+
+// event delegation
+cardTasks.addEventListener("click", (e) => {
+	const target = e.target;
+
+	// Find the closest ancestor button element
+	const deleteButton = target.closest("button#deleteBtn");
+	const editBtn = target.closest("button#editBtn");
+	const checkbox = target.closest("#checkBox");
+
+	if (deleteButton) {
+		// get the id of the btn
+		const id = parseInt(deleteButton.dataset.id);
+		deleteItem(id);
+	} else if (editBtn) {
+		console.log("Edit Btn clicked");
+	} else if (checkbox) {
+		const id = parseInt(checkbox.dataset.id);
+		if (checkbox.checked === true) updateCheckItem(id, true);
+		else updateCheckItem(id, false);
+	}
+});
 
 // display initial data
 displayData();
@@ -104,17 +109,25 @@ function showComplete() {
 			cardTasks.innerHTML += `
             <div class="card__tasks">
                 <div class="card__desc">
-                    <input type="checkbox" name="" id="checkBox" data-id="${data.id}" ${data.isCompleted === true ? 'checked' : ""}>
+                    <input type="checkbox" name="" id="checkBox" data-id="${
+						data.id
+					}" ${data.isCompleted === true ? "checked" : ""}>
                     <input class="card__input-box hidden" type="text" name=" ">
-                    <p class="card__tasks_desc ${data.isCompleted === true ? 'completed' : ''}" >
+                    <p class="card__tasks_desc ${
+						data.isCompleted === true ? "completed" : ""
+					}" >
                         ${data.task}
                     </p>
                 </div>
                 <div class="card__btn__group">
                     <!-- edit button -->
-                    <button class="card__btn small" id="editBtn" data-id="${data.id}"><i class='bx bxs-edit-alt'></i></button>
+                    <button class="card__btn small" id="editBtn" data-id="${
+						data.id
+					}"><i class='bx bxs-edit-alt'></i></button>
                     <!-- delete button -->
-                    <button class="card__btn small" id="deleteBtn" data-id="${data.id}"><i class='bx bxs-trash'></i></button>
+                    <button class="card__btn small" id="deleteBtn" data-id="${
+						data.id
+					}"><i class='bx bxs-trash'></i></button>
                 </div>
             </div>            
             `;
@@ -170,7 +183,7 @@ function deleteItem(id) {
 	}
 }
 
-function updateCheckItem(id, condition) { 
+function updateCheckItem(id, condition) {
 	const index = exampleData.findIndex((todo) => todo.id === id);
 	exampleData[index].isCompleted = condition;
 	displayData();
